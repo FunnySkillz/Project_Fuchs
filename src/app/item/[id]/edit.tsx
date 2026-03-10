f09af8cdae29278efa2efef66a25d0f767eabd8b
FunnySkillz <sebastijan.bogdan@outlook.com>
2026-03-10 11:01:21 +0100

    fix the padding issue top

--- src/app/item/[id]/edit.tsx
+++ src/app/item/[id]/edit.tsx
@@ -864,13 +864,16 @@ export default function ItemEditRoute() {
 
   return (
     <SafeAreaView style={{ flex: 1 }} edges={["bottom"]}>
-      <Box flex={1} px="$5" py="$6">
+      <Box flex={1} px="$5">
         <ScrollView
           ref={scrollRef}
+          contentInsetAdjustmentBehavior="never"
+          automaticallyAdjustContentInsets={false}
           contentContainerStyle={{
             width: "100%",
             maxWidth: 860,
             alignSelf: "center",
+            paddingTop: 24,
             paddingBottom: insets.bottom + 24,
           }}
         >
